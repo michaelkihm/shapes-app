4783b50ce82ce8cb74509b1a9409fe9b43082942
Michael Kihm <Michael.Kihm@rtl.de>
2024-10-25 15:02:05 +0000

    feat: allow several images upload

--- shapes/routers/uploads.py
+++ shapes/routers/uploads.py
@@ -4,12 +4,12 @@ from typing import TypedDict
 
 import cv2
 import numpy as np
-from fastapi import APIRouter, Depends, File, Request, UploadFile
+from fastapi import APIRouter, Depends, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from torch import Tensor
 from ultralytics import YOLO
-from ultralytics.engine.results import Boxes, Results
+from ultralytics.engine.results import Results
 
 from ..dependencies import get_yolo
 
@@ -17,10 +17,21 @@ templates = Jinja2Templates(directory="shapes/templates")
 router = APIRouter()
 
 
-class PredResult(TypedDict):
+class DetResult(TypedDict):
     conf: float
     box: list[int]  # box x,y,w,h
     label: str
+
+
+class CropDetResult(DetResult):
+    img: str
+
+
+type TemplateDets = dict[str, list[CropDetResult]]
+
+
+def imgp_from_index(i: int) -> str:
+    return f"shapes/static/imgs/uploaded_image_{i}.jpg"
 
 
 def convert_xywh_to_box(xywh_box: Tensor) -> list[int]:
@@ -32,29 +43,36 @@ def convert_xywh_to_box(xywh_box: Tensor) -> list[int]:
     return [int(x_c - w / 2), int(y_c - h / 2), int(w), int(h)]
 
 
-def extract_results(
-    boxes: Boxes, names: dict[int, str], conf_thr=0.9
-) -> list[PredResult]:
-    predictions: list[PredResult] = []
-    for box in boxes:
-        conf = float(box.conf)
-        if conf < conf_thr:
-            continue
-        predictions.append(
-            {
-                "conf": round(conf, 2),
-                "box": convert_xywh_to_box(box.xywh),
-                "label": names[int(box.cls)],
-            }
-        )
+def extract_detections(results: list[Results], conf_thr=0.9) -> list[list[DetResult]]:
+    predictions: list[list[DetResult]] = []
+    names = results[0].names
+
+    for result in results:
+        img_redictions: list[DetResult] = []
+        boxes = result.boxes
+        for box in boxes:  # type:ignore
+            conf = float(box.conf)
+            if conf < conf_thr:
+                continue
+            img_redictions.append(
+                {
+                    "conf": round(conf, 2),
+                    "box": convert_xywh_to_box(box.xywh),
+                    "label": names[int(box.cls)],
+                }
+            )
+        predictions.append(img_redictions)
     return predictions
 
 
-def crop_detections(detections: list[PredResult], query_img: np.ndarray) -> list[str]:
+def crop_detections(
+    detections: list[DetResult], query_img: np.ndarray, img_i: int
+) -> list[str]:
     paths: list[str] = []
+
     for i, det in enumerate(detections):
         x, y, w, h = det["box"]
-        path = f"shapes/static/imgs/detection_{i}.jpg"
+        path = f"shapes/static/imgs/detection_{img_i}_{i}.jpg"
         paths.append(path)
         cv2.imwrite(
             path,
@@ -64,38 +82,34 @@ def crop_detections(detections: list[PredResult], query_img: np.ndarray) -> list
     return paths
 
 
+def build_template_detections(imgs_detections: list[list[DetResult]]) -> TemplateDets:
+    result_dict: dict[str, list[CropDetResult]] = {}
+
+    for i, image in enumerate(imgs_detections):
+        src_img = imgp_from_index(i)
+        result_dict[os.path.basename(src_img)] = [
+            {**pred, "img": os.path.basename(det)}
+            for pred, det in zip(image, crop_detections(image, cv2.imread(src_img), i))
+        ]
+    return result_dict
+
+
 @router.post("/upload", response_class=HTMLResponse)
 async def model_inference(
-    request: Request, file: UploadFile = File(...), yolo: YOLO = Depends(get_yolo)
+    request: Request, files: list[UploadFile], yolo: YOLO = Depends(get_yolo)
 ):
-    # Save the uploaded file
-    img_p = "shapes/static/imgs/uploaded_image.jpg"
-    with open(img_p, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+    # Download and save uploaded files
+    for i, file in enumerate(files):
+        img_p = imgp_from_index(i)
+        with open(img_p, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
 
-    results: list[Results] = yolo(img_p)
+    # Model inference
+    results: list[Results] = yolo([imgp_from_index(i) for i in range(len(files))])
+    detections = extract_detections(results)
 
-    assert results[0].boxes
-    predictions = extract_results(results[0].boxes, results[0].names)
-
-    print(
-        [
-            {**pred, "img": os.path.basename(det)}
-            for pred, det in zip(
-                predictions, crop_detections(predictions, cv2.imread(img_p))
-            )
-        ]
-    )
+    # Init and return HTML template
     return templates.TemplateResponse(
         "results.html",
-        {
-            "request": request,
-            "predictions": [
-                {**pred, "img": os.path.basename(det)}
-                for pred, det in zip(
-                    predictions, crop_detections(predictions, cv2.imread(img_p))
-                )
-            ],
-            "original_image": os.path.basename(img_p),
-        },
+        {"request": request, "detections": build_template_detections(detections)},
     )
